33d0f4621c90da55166f68d56652fc61dda3ec92
iamplacard <iplacard@gmail.com>
2018-11-29 10:18:38 +0900

    Create firstCalculator-02.py
    program for simple calculator

---
+++ firstCalculator-02.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+result = a + b
+print(a,"+",b,"=",result)
+result = a - b
+print(a,"-",b,"=",result)
+result = a * b
+print(a,"*",b,"=",result)
+result = a / b
+print(a,"/",b,"=",result)
