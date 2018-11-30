103f1ef5e7abb393cc897642b2465b5c1b7e7421
iamplacard <iplacard@gmail.com>
2018-11-30 17:02:49 +0900

    Create diamond-01.py
    Diamond 모양 출력 - Print 연습 - No for loop

---
+++ diamond-01.py
@@ -0,0 +1,23 @@
+## Diamon Star ##
+## Diamond 모양 출력
+
+## 함수 선언 ##
+
+def p() :
+       print("*")
+
+## 변수 선언 ##
+
+## Main Code ##
+
+
+print("%d" % 123)
+print("%5d" % 123)
+print("%05d" % 123)
+
+print("%f" % 123.44)
+print("%7.1f" % 123.45)
+print("%7.3f" % 123.45)
+
+print("%s" % "Python")
+print("%10s" % "Python")
