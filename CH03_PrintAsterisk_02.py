f908db217ff17cf664559add08d48807f5e1accb
iamplacard <iplacard@gmail.com>
2019-11-19 11:52:22 +0900

    Create CH03_PrintAsterisk_02.py
    Just print asterisk

---
+++ CH03_PrintAsterisk_02.py
@@ -0,0 +1,23 @@
+## print Diamond shape ##
+
+## 함수 선언 ##
+
+## 변수 선언 ##
+
+## Main Code ##
+print("%5s" % "*")
+print("%6s" % "***")
+print("%7s" % "*****")
+print("%8s" % "*******")
+print("%9s" % "*********")
+print("%8s" % "*******")
+print("%7s" % "*****")
+print("%6s" % "***")
+print("%5s" % "*")
+
+print("\n 줄 바꾸기")
+print("\n 줄 바꾸기")
+
+for x in range(0, 9) :
+    print(" ")
+    print("*")
