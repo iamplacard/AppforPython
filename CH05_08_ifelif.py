5b4ee233db7e125ccdfae663f22b44f3db4d3637
iamplacard <iplacard@gmail.com>
2019-11-19 14:36:56 +0900

    Create CH05_08_ifelif.py
    if_elif example

---
+++ CH05_08_ifelif.py
@@ -0,0 +1,24 @@
+## leap Year example ##
+'''
+
+'''
+
+## 함수 선언 ##
+
+## 변수 선언 ##
+
+## Main Code ##
+if __name__ == "__main__" :
+    score = int(input("점수를 입려하세요 :"))
+    if score >= 90 :
+        print("A")
+    elif score >=80 :
+        print('B')
+    elif score >= 70 :
+        print('C')
+    elif score >= 60 :
+        print('D')
+    else :
+        print('F')
+
+    print("학점입니다.")
