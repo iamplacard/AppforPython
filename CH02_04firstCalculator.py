e8d87ca9fbfbc18be8bd4166b21c5a590047c4c6
iamplacard <iplacard@gmail.com>
2019-11-19 11:19:46 +0900

    Create CH02_04firstCalculator.py
    First calculator with text input function

---
+++ CH02_04firstCalculator.py
@@ -0,0 +1,11 @@
+a = int(input("Please Type first input : "))
+b = int(input("Please Type second input : "))
+
+result = a + b
+print(a,"+",b,"=",result)
+result = a - b
+print(a,"-",b,"=",result)
+result = a * b
+print(a,"*",b,"=",result)
+result = a / b
+print(a,"/",b,"=",result)
