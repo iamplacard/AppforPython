2627f227905630e399b580359ef5cd445e6251f4
iamplacard <iplacard@gmail.com>
2019-11-19 15:47:33 +0900

    Create CH09_05_Module_Ex_a.py
    Module example a

---
+++ CH09_05_Module_Ex_a.py
@@ -0,0 +1,16 @@
+##  모듈에 대하여 ... - 함수의 집
+## 모듈 호출 방법은
+##  import 모듈이름 (Ch09_05_Module)
+##  사용 - Ch09_05_Module.func1() 으로 사용
+
+import Ch09_05_Module
+
+## 함수 선언 ##
+
+##  변수 선언   ##
+
+## Main Code ##
+if __name__ == "__main__" :
+    Ch09_05_Module.func1()
+    Ch09_05_Module.func2()
+    Ch09_05_Module.func3()
