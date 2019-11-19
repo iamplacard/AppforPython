a1d8b1acd70483a684d042317312d5906ddd3501
iamplacard <iplacard@gmail.com>
2019-11-19 11:30:34 +0900

    Create CH02_Turtle_ex01.py
    Turtle graphic example with several cursor shape

---
+++ CH02_Turtle_ex01.py
@@ -0,0 +1,32 @@
+## tutle grapic Program ##
+'''
+   Comment for many lines
+   Many lines
+'''
+
+'''
+   def  함수명(매개변수) :
+       global 사용할_전역_변수
+       # 이 부분에 함수 내용을 코딩
+'''
+
+import turtle
+
+## 함수 선언 ##
+
+## 변수 선언 ##
+
+## Main Code ##
+# turtle.shape('turtle')
+# turtle.shape('classic')
+turtle.shape('triangle')
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+
+turtle.done()
